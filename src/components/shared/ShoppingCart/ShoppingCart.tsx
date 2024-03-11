4bf9bc9c2e9c957ea24ce098764b7930e853d62a
Xavi Flores <xa_franc@live.com>
2024-03-10 21:59:44 -0500

    Add zustand to handled state to shopping cart

--- src/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -5,12 +5,22 @@ import {useShoppingCart} from "app/hooks/useShoppingCart";
 
 export const ShoppingCart = () => {
 const {cart} = useShoppingCart();
+const [isOpen,setIsOpen]= useState(false);
+const handleOpen = () => setIsOpen(!isOpen);
+
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>
         {cart.length}
       </span>
       <FaShoppingCart />
+        <div className={styles.ShoppingCart__items}>
+            {
+                cart.map(item =>(
+                    <p key={item?.id}>{item?.title}</p>
+                ))
+            }
+        </div>
     </button>
   )
 }
